ab91b19819163b256062319276788240b6868b4c
delindell <daviselindell@gmail.com>
2020-02-10 22:28:50 -0600

    forgot to save JS

--- main.js
+++ main.js
@@ -1,1 +1,56 @@
-console.log('whats gucci?')
+const merchItems = [
+    {
+        type: 'hat',
+        title: 'Xcalibur',
+        price: 5000,
+        imgUrl: 'https://images.unsplash.com/photo-1526478806334-5fd488fcaabc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=790&q=80',
+    },
+    {
+        type: 'hat',
+        title: 'Feast of the Three Amigos',
+        price: 5000,
+        imgUrl: 'https://images.unsplash.com/photo-1526478806334-5fd488fcaabc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=790&q=80',
+    },
+    {
+        type: 'hat',
+        title: 'Don\'t Start Nothin\' Wont\'t Be Nothin\'',
+        price: 5000,
+        imgUrl: 'https://images.unsplash.com/photo-1526478806334-5fd488fcaabc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=790&q=80',
+    },
+    {
+        type: 'tShirt',
+        title: 'Zoe T-Shirt',
+        price: 5000,
+        imgUrl: 'https://images.unsplash.com/photo-1526478806334-5fd488fcaabc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=790&q=80',
+    },
+    {
+        type: 'tShirt',
+        title: 'Luke T-Shirt',
+        price: 5000,
+        imgUrl: 'https://images.unsplash.com/photo-1526478806334-5fd488fcaabc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=790&q=80',
+    },
+    {
+        type: 'tShirt',
+        title: 'Mary T-Shirt',
+        price: 5000,
+        imgUrl: 'https://images.unsplash.com/photo-1526478806334-5fd488fcaabc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=790&q=80',
+    },
+    {
+        type: 'hat',
+        title: 'Zoe Hat',
+        price: 5000,
+        imgUrl: 'https://images.unsplash.com/photo-1526478806334-5fd488fcaabc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=790&q=80',
+    },
+    {
+        type: 'hat',
+        title: 'Mary Hat',
+        price: 5000,
+        imgUrl: 'https://images.unsplash.com/photo-1526478806334-5fd488fcaabc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=790&q=80',
+    },
+    {
+        type: 'hat',
+        title: 'Luke Hat',
+        price: 5000,
+        imgUrl: 'https://images.unsplash.com/photo-1526478806334-5fd488fcaabc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=790&q=80',
+    }
+];
