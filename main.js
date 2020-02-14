3c0e18942918bb95de53ce6f193d293ab5b9054e
Sarah Holder <sr.holder@yahoo.com>
2020-02-13 21:06:23 -0600

    wip

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
-<<<<<<< HEAD
 // const tours = [ 
-=======
-const tours = [ 
->>>>>>> 072778de7c596262b40cefb16a8195eff3a45a3a
-
 //     {date: 'Sun, Feb, 23rd', venue:'For Solo', location: 'Mexico Mexico'},
 //     {date: 'Sun, Feb, 24th', venue:'For Solo', location:  'Mexico Mexico'},
 //     {date: 'Thu, Feb, 27th', venue:'Festival Del Mar', location: 'Vina Del Chile'},
 //     {date: 'Fri, Feb, 28th', venue:'Pepsi Center', location: 'Denver, CO'},
 //     {date: 'Sun, May, 31st', venue:'Chin Pavilion', location:  'Phoenix, AZ'},
 //     {date: 'Wed, Jun, 3rd', venue:'Wrigley Field', location: 'Chicago, IL'},
-//     {date: 'Wed, Jun, 24th', venue:'Fenway Park', location: 'Boston, MA'},
-    
-<<<<<<< HEAD
+//     {date: 'Wed, Jun, 24th', venue:'Fenway Park', location: 'Boston, MA'},   
 // ];
-
 // const merchItems = [
 //     {
 //         type: 'album',
@@ -207,153 +199,147 @@ const albumBuilder = (builderArr)=> {
         domString +=      `</div>`;
         domString +=   `</div>`;
         domString += `</div>`
-=======
-];
-
-const merchItems = [
-    {
-        type: 'album',
-        title: 'Xcalibur',
-        price: 5000,
-        imgUrl: 'https://images.unsplash.com/photo-1526478806334-5fd488fcaabc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=790&q=80',
-        buttonId: 'a'
-    },
-    {
-        type: 'album',
-        title: 'Feast of the Three Amigos',
-        price: 5000,
-        imgUrl: 'https://images.unsplash.com/photo-1526478806334-5fd488fcaabc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=790&q=80',
-        buttonId: 'b'
-    },
-    {
-        type: 'album',
-        title: 'Don\'t Start Nothin\' Won\'t Be Nothin\'',
-        price: 5000,
-        imgUrl: 'https://images.unsplash.com/photo-1526478806334-5fd488fcaabc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=790&q=80',
-        buttonId: 'c'
-    },    
-    {
-        type: 'tShirt',
-        title: 'Zoe T-Shirt',
-        price: 5000,
-        imgUrl: 'https://i.imgur.com/r9tz26G.png',
-        buttonId: 'd'
-    },
-    {
-        type: 'tShirt',
-        title: 'Luke T-Shirt',
-        price: 5000,
-        imgUrl: 'https://i.imgur.com/EA34ZF7.png',
-        buttonId: 'e'
-    },
-    {
-        type: 'tShirt',
-        title: 'Mary T-Shirt',
-        price: 5000,
-        imgUrl: 'https://i.imgur.com/OiAaEap.png',
-        buttonId: 'f'
-    },
-    {
-        type: 'hat',
-        title: 'Zoe Hat',
-        price: 5000,
-        imgUrl: 'https://i.imgur.com/GY6iLC6.jpg',
-        buttonId: '3'
-    },
-    {
-        type: 'hat',
-        title: 'Mary Hat',
-        price: 5000,
-        imgUrl: 'https://i.imgur.com/0pStUHE.png',
-        buttonId: '2'
-    },
-    {
-        type: 'hat',
-        title: 'Luke Hat',
-        price: 5000,
-        imgUrl: 'https://i.imgur.com/MhCy87C.png',
-        id: '1'
-    }
-];
-
-const printToDom = (divId, textToPrint) => {
-    const selectedDiv = document.getElementById(divId);
-    selectedDiv.innerHTML = textToPrint;
-  };
-
-const buildTourCards = () => {
-    let domString = '';
-    for(let i = 0; i < tours.length; i++) {
-      domString += '<div class="event">';
-      domString +=   `<span class="date">${tours[i].date}</span>`;
-      domString += `<div class="details">`;
-      domString +=   `<div class="venue">${tours[i].venue}</div>`;
-      domString +=   `<div class="location">${tours[i].location}</div>`;
-      domString += `</div>`;
-      domString += '</div>';
-    };
-    printToDom('tour-dates', domString);
-  };
-
-const merchPrinter = (lastArray) => {
-    let domString = '';
-    for (let i = 0; i < lastArray.length; i++) {
-        domString += '<div class="col-md-6 col-lg-4">'
-        domString += '<div class="individualCards card" style="width: 20rem;">';
-        domString +=    `<img src="${lastArray[i].imgUrl}" class="card-img-top rounded merchImages" alt="...">`
-        domString +=    '<div class="card-body">'
-        domString +=       `<h5 class="card-title d-flex justify-content-center">${lastArray[i].title}</h5>`
-        domString +=       `<p class="card-price d-flex justify-content-center">$   ${lastArray[i].price}</p>`
-        if (lastArray[i].type === 'tShirt' || lastArray[i].type === 'hat') {
-            domString += '<div class="dropdown text-center">'
-            domString += '<button class="btn btn-danger dropdown-toggle mb-1" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Size</button>'
-            domString += '<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">'
-            domString +=    '<a class="dropdown-item">Small</a>'
-            domString +=    '<a class="dropdown-item">Medium</a>'
-            domString +=    '<a class="dropdown-item">Large</a>'
-            domString += '</div>'
-            domString += '</div>'
-        };
-        domString +=       `<div class="text-center"><button class="btn btn-danger text-center purchaseButton" id="${merchItems[i].buttonId}">Purchase</button></div>`;
-        domString +=    '</div>'
-        domString +=    '</div>'
-        domString += '</div>'
->>>>>>> 072778de7c596262b40cefb16a8195eff3a45a3a
     };
     printToDom('album-list', domString);
-    activateButton();
-};
+}
+albumBuilder(albumArr);
+
+// const merchItems = [
+//     {
+//         type: 'album',
+//         title: 'Xcalibur',
+//         price: 5000,
+//         imgUrl: 'https://images.unsplash.com/photo-1526478806334-5fd488fcaabc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=790&q=80',
+//         buttonId: 'a'
+//     },
+//     {
+//         type: 'album',
+//         title: 'Feast of the Three Amigos',
+//         price: 5000,
+//         imgUrl: 'https://images.unsplash.com/photo-1526478806334-5fd488fcaabc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=790&q=80',
+//         buttonId: 'b'
+//     },
+//     {
+//         type: 'album',
+//         title: 'Don\'t Start Nothin\' Won\'t Be Nothin\'',
+//         price: 5000,
+//         imgUrl: 'https://images.unsplash.com/photo-1526478806334-5fd488fcaabc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=790&q=80',
+//         buttonId: 'c'
+//     },    
+//     {
+//         type: 'tShirt',
+//         title: 'Zoe T-Shirt',
+//         price: 5000,
+//         imgUrl: 'https://i.imgur.com/r9tz26G.png',
+//         buttonId: 'd'
+//     },
+//     {
+//         type: 'tShirt',
+//         title: 'Luke T-Shirt',
+//         price: 5000,
+//         imgUrl: 'https://i.imgur.com/EA34ZF7.png',
+//         buttonId: 'e'
+//     },
+//     {
+//         type: 'tShirt',
+//         title: 'Mary T-Shirt',
+//         price: 5000,
+//         imgUrl: 'https://i.imgur.com/OiAaEap.png',
+//         buttonId: 'f'
+//     },
+//     {
+//         type: 'hat',
+//         title: 'Zoe Hat',
+//         price: 5000,
+//         imgUrl: 'https://i.imgur.com/GY6iLC6.jpg',
+//         buttonId: '3'
+//     },
+//     {
+//         type: 'hat',
+//         title: 'Mary Hat',
+//         price: 5000,
+//         imgUrl: 'https://i.imgur.com/0pStUHE.png',
+//         buttonId: '2'
+//     },
+//     {
+//         type: 'hat',
+//         title: 'Luke Hat',
+//         price: 5000,
+//         imgUrl: 'https://i.imgur.com/MhCy87C.png',
+//         id: '1'
+//     }
+// ];
+
+// const printToDom = (divId, textToPrint) => {
+//     const selectedDiv = document.getElementById(divId);
+//     selectedDiv.innerHTML = textToPrint;
+//  };
+
+// const buildTourCards = () => {
+//     let domString = '';
+//     for(let i = 0; i < tours.length; i++) {
+//       domString += '<div class="event">';
+//       domString +=   `<span class="date">${tours[i].date}</span>`;
+//       domString += `<div class="details">`;
+//       domString +=   `<div class="venue">${tours[i].venue}</div>`;
+//       domString +=   `<div class="location">${tours[i].location}</div>`;
+//       domString += `</div>`;
+//       domString += '</div>';
+//     };
+//     printToDom('tour-dates', domString);
+//   };
+
+// const merchPrinter = (lastArray) => {
+//     let domString = '';
+//     for (let i = 0; i < lastArray.length; i++) {
+//         domString += '<div class="col-md-6 col-lg-4">'
+//         domString += '<div class="individualCards card" style="width: 20rem;">';
+//         domString +=    `<img src="${lastArray[i].imgUrl}" class="card-img-top rounded merchImages" alt="...">`
+//         domString +=    '<div class="card-body">'
+//         domString +=       `<h5 class="card-title d-flex justify-content-center">${lastArray[i].title}</h5>`
+//         domString +=       `<p class="card-price d-flex justify-content-center">$   ${lastArray[i].price}</p>`
+//         if (lastArray[i].type === 'tShirt' || lastArray[i].type === 'hat') {
+//             domString += '<div class="dropdown text-center">'
+//             domString += '<button class="btn btn-danger dropdown-toggle mb-1" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Size</button>'
+//             domString += '<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">'
+//             domString +=    '<a class="dropdown-item">Small</a>'
+//             domString +=    '<a class="dropdown-item">Medium</a>'
+//             domString +=    '<a class="dropdown-item">Large</a>'
+//             domString += '</div>'
+//             domString += '</div>'
+//         };
+//         domString +=       `<div class="text-center"><button class="btn btn-danger text-center purchaseButton" id="${merchItems[i].buttonId}">Purchase</button></div>`;
+//         domString +=    '</div>'
+//         domString +=    '</div>'
+//         domString += '</div>'
+//     };
+//     printToDom('album-list', domString);
+    // activateButton();
+// };
 const activateButton = () =>{ 
     let getButton = document.getElementsByClassName("trackButton");
 for (let i = 0; i < getButton.length; i++) {
     getButton[i].addEventListener('click', trackBuilder);
 }; 
 }
-    albumBuilder(albumArr);
-
-    
-
-<<<<<<< HEAD
-
-=======
-const purchaseMerch = () => {
-    alert("This item has been to your cart!");
-};
-
-const merchSorter = (e) => {
-    const sortedMerch = [];
-    const buttonId = e.target.id;
-    if(buttonId === 'all') {
-        merchPrinter(merchItems);
-    } else {
-        for (let i = 0; i < merchItems.length; i++) {
-            if (merchItems[i].type === buttonId) {
-                sortedMerch.push(merchItems[i]);
-            }; 
-        };
-        merchPrinter(sortedMerch);
-    };
-};
+//     albumBuilder(albumArr);
+// const purchaseMerch = () => {
+//     alert("This item has been to your cart!");
+// };
+// const merchSorter = (e) => {
+//     const sortedMerch = [];
+//     const buttonId = e.target.id;
+//     if(buttonId === 'all') {
+//         merchPrinter(merchItems);
+//     } else {
+//         for (let i = 0; i < merchItems.length; i++) {
+//             if (merchItems[i].type === buttonId) {
+//                 sortedMerch.push(merchItems[i]);
+//             }; 
+//         };
+//         merchPrinter(sortedMerch);
+//     };
+// };
 
 // const footerBuilder = () => {
 //     let domString = '';
@@ -362,29 +348,24 @@ const merchSorter = (e) => {
 //     printToDom ('footer-text', domString);
 // }
 // footerBuilder();
+// const event = () => {
+//     document.getElementById('album').addEventListener('click', merchSorter);
+//     document.getElementById('tShirt').addEventListener('click', merchSorter);
+//     document.getElementById('hat').addEventListener('click', merchSorter);
+//     document.getElementById('all').addEventListener('click', merchSorter);
+// };
+// const init = () => {
 
-const event = () => {
-    document.getElementById('album').addEventListener('click', merchSorter);
-    document.getElementById('tShirt').addEventListener('click', merchSorter);
-    document.getElementById('hat').addEventListener('click', merchSorter);
-    document.getElementById('all').addEventListener('click', merchSorter);
-};
-
-const init = () => {
-
-    // if (window.location.pathname == 'bands-scuurvy/merchandise.html'){
-    merchPrinter(merchItems)    
-    event();
+//     // if (window.location.pathname == 'bands-scuurvy/merchandise.html'){
+//     merchPrinter(merchItems)    
+//     event();
     
-    // if (window.location.pathname === '.merchandise.html'){
-    merchPrinter(merchItems); 
-    event();
+//     // if (window.location.pathname === '.merchandise.html'){
+//     merchPrinter(merchItems); 
+//     event();
 
-    // };
-    // if (window.location.pathname === '/.tour.html') {
-    //     buildTourCards();
-    // };
-};
->>>>>>> 072778de7c596262b40cefb16a8195eff3a45a3a
-
-init();
+//     // };
+//     // if (window.location.pathname === '/.tour.html') {
+//     //     buildTourCards();
+//     // };
+// };
